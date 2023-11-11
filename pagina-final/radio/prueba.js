c1cb9e955932cf9a6e4f246dd7620e6396f65e68
Espinosa Lucas <127991954+Lucas-rafa@users.noreply.github.com>
2023-11-10 21:19:23 -0300

    Delete pagina-final directory

--- pagina-final/radio/prueba.js
+++
@@ -1,5 +0,0 @@
-const navToggle = document.getElementById("nav-toggle");
-  const navLinks = document.querySelector(".nav-links");
-
-  navToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");  });
